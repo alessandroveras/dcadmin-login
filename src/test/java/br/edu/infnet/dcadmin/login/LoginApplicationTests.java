9408bf57b1dc18b67258c3d35b474f8d4c296b54
alessandroveras <alessandro.veras@gmail.com>
2022-08-24 01:12:06 -0300

    Project skeleton

---
+++ src/test/java/br/edu/infnet/dcadmin/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.dcadmin.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
